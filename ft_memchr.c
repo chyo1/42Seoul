126cebc0b6eeffffbfe7643a810363ef5f482104
Hyowon Choi <hyowchoi@c2r6s6.42seoul.kr>
2023-10-11 18:45:17 +0900

    fix_ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hyowchoi <hyowchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 11:39:53 by hyowchoi          #+#    #+#             */
-/*   Updated: 2023/10/11 14:20:24 by hyowchoi         ###   ########.fr       */
+/*   Updated: 2023/10/11 18:43:32 by hyowchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
 	size_t			i;
+	unsigned char	cmp;
 
+	cmp = (unsigned char)c;
 	str = (unsigned char *)s;
 	i = 0;
 	while (str[i] && i < n)
 	{
-		if (str[i] == (unsigned char)c)
+		if (str[i] == cmp)
 			return (str + i);
 		i++;
 	}
-	if (c == 0)
+	if (cmp == 0)
 		return (str + i);
 	return (NULL);
 }
