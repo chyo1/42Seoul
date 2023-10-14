59740606eb8a248e7d9518ca1a3d6ee566d85256
Hyowon Choi <hyowchoi@c2r6s6.42seoul.kr>
2023-10-14 14:15:43 +0900

    1013_1414

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hyowchoi <hyowchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 15:29:18 by hyowchoi          #+#    #+#             */
-/*   Updated: 2023/10/14 12:21:21 by hyowchoi         ###   ########.fr       */
+/*   Updated: 2023/10/14 14:02:48 by hyowchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,6 @@ static unsigned int	get_start(char const *s1, char const *set, unsigned int end)
 
 static unsigned int	get_end(char const *s1, char const *set, unsigned int end)
 {
-	if (end == (unsigned int)-1)
-		return (0);
 	while (0 < end)
 	{
 		if (!find_set(s1[end], set))
@@ -59,12 +57,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	len = ft_strlen(s1);
 	start = get_start(s1, set, len);
-	end = get_end(s1, set, len - 1);
 	if (s1[0] == '\0' || start == len)
 		return ((char *)ft_calloc(1, sizeof(char)));
+	end = get_end(s1, set, len - 1);
 	str = (char *)malloc(sizeof(char) * (end - start + 2));
 	if (str == NULL)
 		return (NULL);
-	strlcpy(str, (s1 + start), end - start + 2);
+	ft_strlcpy(str, (s1 + start), end - start + 2);
 	return (str);
 }
