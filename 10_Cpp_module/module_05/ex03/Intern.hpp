5383570dc240f469f60092fa9096e31a0ec28e04
chyo1 <hyowon3856@gmail.com>
2024-07-23 22:04:02 +0900

    05_ex03 [ADD] draft

---
+++ 10_Cpp_module/module_05/ex03/Intern.hpp
@@ -0,0 +1,12 @@
+#ifndef INTERN_HPP
+#define INTERN_HPP
+
+#include <iostream>
+#include "AForm.hpp"
+
+class Intern {
+    public:
+
+};
+
+#endif
--- 10_Cpp_module/module_05/ex03/main.cpp
+++ 10_Cpp_module/module_05/ex03/main.cpp
@@ -7,54 +7,5 @@
 
 int main() {
 
-    try {
-        Bureaucrat b1("b1", 1);
-        ShrubberyCreationForm shrubbery("home");
-        shrubbery.beSigned(b1);
-        shrubbery.execute(b1);
-
-    } catch (const AForm::GradeTooHighException& e) {
-        std::cout << e.what() << std::endl;
-    } catch (const AForm::GradeTooLowException& e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    try {
-        Bureaucrat b2("b2", 1);
-        RobotomyRequestForm robotomy("home2");
-        robotomy.beSigned(b2);
-        robotomy.execute(b2);
-
-    } catch (const AForm::GradeTooHighException& e) {
-        std::cout << e.what() << std::endl;
-    } catch (const AForm::GradeTooLowException& e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    try {
-        Bureaucrat b3("b3", 1);
-        PresidentialPardonForm presidentialPardon("home3");
-        presidentialPardon.beSigned(b3);
-        presidentialPardon.execute(b3);
-
-    } catch (const AForm::GradeTooHighException& e) {
-        std::cout << e.what() << std::endl;
-    } catch (const AForm::GradeTooLowException& e) {
-        std::cout << e.what() << std::endl;
-    }
-
-
-    try {
-        Bureaucrat b4("b4", 2);
-        ShrubberyCreationForm shrubbery("home4");
-        shrubbery.beSigned(b4);
-        b4.executeForm(shrubbery);
-
-    } catch (const AForm::GradeTooHighException& e) {
-        std::cout << e.what() << std::endl;
-    } catch (const AForm::GradeTooLowException& e) {
-        std::cout << e.what() << std::endl;
-    }
-
     return 0;
 }
