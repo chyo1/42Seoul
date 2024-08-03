a5a3f009c7f57a6a685976598ec77de70c939411
chyo1 <hyowon3856@gmail.com>
2024-08-04 01:49:01 +0900

    07_ex01 [FIN] Func template don't implement..

--- 10_Cpp_module/module_07/ex01/iter.hpp
+++ 10_Cpp_module/module_07/ex01/iter.hpp
@@ -1,9 +1,14 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-#include <cstdlib>
-
 // 함수 포인터 템플릿으로 정의해서 넘기기 ->  const 필요 없음
+// template<typename T, typename Func>
+// void iter(T *array, size_t length, Func func) {
+//     for (size_t i = 0; i < length; i++) {
+//         func(array[i]);
+//     }
+// }
+
 template<typename T>
 void iter(T *array, size_t length, void (*func)(T &)) {
     for (size_t i = 0; i < length; i++) {
@@ -17,5 +22,6 @@ void iter(const T *array, size_t length, void (*func)(const T&)) {
         func(array[i]);
     }
 }
+
 
 #endif
