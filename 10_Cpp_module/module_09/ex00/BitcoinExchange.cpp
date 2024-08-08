24c5cefa7a79d6d268908d76a2091db73a9a2a64
chyo1 <hyowon3856@gmail.com>
2024-08-08 20:40:45 +0900

    09_ex00 [FIX] bad input

--- 10_Cpp_module/module_09/ex00/BitcoinExchange.cpp
+++ 10_Cpp_module/module_09/ex00/BitcoinExchange.cpp
@@ -17,7 +17,21 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other) {
 
 BitcoinExchange::~BitcoinExchange() {}
 
+bool isDateAllDigits(const std::string &str) {
+    for (size_t i = 0; i < str.size(); i++) {
+        if (!isdigit(str[i]))
+            return false;
+    }
+    return true;
+}
 
+bool isRateAllDigits(const std::string &str) {
+    for (size_t i = 0; i < str.size(); i++) {
+        if (!isdigit(str[i]) && str[i] != '.' && str[0] != '-')
+            return false;
+    }
+    return true;
+}
 
 void BitcoinExchange::readDataFileAndGetRate() {
     std::ifstream file("data.csv");
@@ -37,7 +51,7 @@ void BitcoinExchange::readDataFileAndGetRate() {
             
             std::string date = line.substr(0, idx);
             std::string rate = line.substr(idx + 1, line.size() - idx - 1);
-            
+            // std::cout << date << " " << rate << std::endl;
             checkValidDate(date);
             checkValidRate(rate);
 
@@ -53,6 +67,10 @@ void BitcoinExchange::checkValidDate(std::string date) {
     if (date < "2009-01-02")
         throw std::runtime_error("Invalid date");
     int year, month, day;
+
+    if (!isDateAllDigits(date.substr(0, 4)) || !isDateAllDigits(date.substr(5, 2)) || !isDateAllDigits(date.substr(8, 2)))
+        throw std::runtime_error("Invalid date");
+
     year = stoi(date.substr(0, 4));
     month = stoi(date.substr(5, 2));
     day = stoi(date.substr(8, 2));
@@ -75,7 +93,7 @@ void BitcoinExchange::checkValidDate(std::string date) {
 }
 
 void BitcoinExchange::checkValidRate(std::string rate) {
-    if (rate.size() == 0 || stof(rate) < 0)
+    if (rate.size() == 0 || !isRateAllDigits(rate) || stof(rate) < 0)
         throw std::runtime_error("Invalid exchange rate");
 }
 
@@ -99,12 +117,24 @@ float BitcoinExchange::openInputFileAndGetBitcoinPrice(char* fileName) {
         }
         
         std::string date = line.substr(0, idx - 1);
-        float value = stof(line.substr(idx + 2, line.size() - idx - 2));
         
         try {
             checkValidDate(date);
         } catch (const std::exception &e) {
             std::cout << "Error: bad input => " << date << std::endl;
+            continue;
+        }
+
+        if (!isRateAllDigits(line.substr(idx + 2, line.size() - idx - 2))) {
+            std::cout << "Error: bad input => " << line.substr(idx + 2, line.size() - idx - 2) << std::endl;
+            continue;
+        }
+
+        float value = 0;
+        try {
+            value = stof(line.substr(idx + 2, line.size() - idx - 2));
+        } catch (const std::exception &e) {
+            std::cout << "Error: bad input => " << line.substr(idx + 2, line.size() - idx - 2) << std::endl;
             continue;
         }
         if (value < 0) {
@@ -124,9 +154,6 @@ float BitcoinExchange::openInputFileAndGetBitcoinPrice(char* fileName) {
     return 0;
 }
 
-// bool cmp(const std::pair<std::string, float>& pair, const std::string& key) {
-//     return pair.first <= key;
-// }
 
 float BitcoinExchange::getBitcoinPrice(std::string date) {
     std::map<std::string, float>::iterator iter = rates.lower_bound(date);
