5a6e7b8e2dd4ff9a1944528ef6d9c289dbae4319
chyo1 <hyowon3856@gmail.com>
2024-08-09 13:43:12 +0900

    09_ex01 [FIN]

---
+++ 10_Cpp_module/module_09/ex01/RPN.cpp
@@ -0,0 +1,67 @@
+#include "RPN.hpp"
+#include <iostream> //
+RPN::RPN() {}
+
+RPN::RPN(const RPN &other) {
+    _s = other._s;
+}
+
+RPN &RPN::operator=(const RPN &other) {
+    _s = other._s;
+    return *this;
+}
+
+RPN::~RPN() {}
+
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+long long RPN::run(std::string str) {
+
+    size_t i = -1;
+    while (str[++i] == ' ');
+
+    for (; i < str.size(); i++) {
+        if (isdigit(str[i])) {
+            _s.push(str[i] - '0');
+            // std::cout << "pushed: " << str[i] - '0' << std::endl;
+        }
+        else if (str[i] == ' ')
+            continue;
+        else if (isOperator(str[i])) {
+            _s.push(calculate(str[i]));
+        }
+        else {
+            throw std::invalid_argument("Error");
+        }
+    }
+    if (_s.size() != 1)
+        throw std::invalid_argument("Error");
+    return _s.top();
+}
+
+long long RPN::calculate(char oper) {
+    if (_s.size() < 2)
+        throw std::invalid_argument("Error");
+
+    long long b = _s.top();
+    _s.pop();
+
+    long long a = _s.top();
+    _s.pop();
+
+    if (oper == '+')
+        return a + b;
+    else if (oper == '-')
+        return a - b;
+    else if (oper == '*')
+        return a * b;
+    else if (oper == '/') {
+        if (b == 0)
+            throw std::invalid_argument("Division by zero");
+        return a / b;
+    }
+    return 0; //
+}
+
---
+++ 10_Cpp_module/module_09/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "RPN.hpp"
+#include <iostream>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "usage: ./RPN \"[expression]\"" << std::endl;
+        return 1;
+    }
+
+    RPN rpn;
+    try {
+        std::cout << rpn.run(argv[1]) << std::endl;
+    } catch (std::invalid_argument &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    
+    return 0;
+}
