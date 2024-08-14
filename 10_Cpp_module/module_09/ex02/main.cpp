9ac261763c13c84b39e4a4b52f5ef1ae20179a4d
chyo1 <hyowon3856@gmail.com>
2024-08-14 18:19:29 +0900

    09_ex01 [ADD] comments

--- 10_Cpp_module/module_09/ex02/main.cpp
+++ 10_Cpp_module/module_09/ex02/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char** argv) {
     std::cout << "Before: ";
     pmergeMe.printArr();
 
+    // std::clock() : 프로그램이 실행된 후의 CPU 시간을 반환, 반환값은 시스템이 시작된 이후로 소모된 클럭 틱의 수를 나타냄
     std::clock_t startVecSec = std::clock();
     pmergeMe.mergeSortVec();
     std::clock_t endVecSec = std::clock();
@@ -25,8 +26,11 @@ int main(int argc, char** argv) {
     std::cout << "After: ";
     pmergeMe.printVec();
 
+    // 시간 단위로 변환
     double vecTime = static_cast<double> (endVecSec - startVecSec) / CLOCKS_PER_SEC;
+    // 백만 곱하기 -> 마이크로초로 변환
     vecTime *= 1e6;
+
     double listTime = static_cast<double> (endListSec - startListSec) / CLOCKS_PER_SEC;
     listTime *= 1e6;
 
