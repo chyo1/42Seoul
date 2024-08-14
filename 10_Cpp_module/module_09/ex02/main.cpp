21ce99d4926e9b09469efd8b69499a5e31f02431
chyo1 <hyowon3856@gmail.com>
2024-08-14 14:47:14 +0900

    09_ex02 [FIN]

--- 10_Cpp_module/module_09/ex02/main.cpp
+++ 10_Cpp_module/module_09/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "PmergeMe.hpp"
 #include <iostream>
+#include <ctime>
 
 int main(int argc, char** argv) {
     PmergeMe pmergeMe;
@@ -13,12 +14,23 @@ int main(int argc, char** argv) {
     std::cout << "Before: ";
     pmergeMe.printArr();
 
+    std::clock_t startVecSec = std::clock();
     pmergeMe.mergeSortVec();
+    std::clock_t endVecSec = std::clock();
+
+    std::clock_t startListSec = std::clock();
+    pmergeMe.mergeSortList();
+    std::clock_t endListSec = std::clock();
 
     std::cout << "After: ";
     pmergeMe.printVec();
 
-    // std::cout << "Time to process a range of " << argc - 1 << "elements with std::vector :" << std::endl;
-    // std::cout << "Time to process a range of " << argc - 1 << "elements with std::[..] :" << std::endl; //
+    double vecTime = static_cast<double> (endVecSec - startVecSec) / CLOCKS_PER_SEC;
+    vecTime *= 1e6;
+    double listTime = static_cast<double> (endListSec - startListSec) / CLOCKS_PER_SEC;
+    listTime *= 1e6;
+
+    std::cout << "Time to process a range of " << argc - 1 << " elements with std::vector : " << vecTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << argc - 1 << " elements with std::list   : " << listTime << " us" << std::endl;
 
 }
