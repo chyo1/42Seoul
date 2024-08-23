65014b1fde6f568e10749542d9ebb0b7d0993afe
chyo1 <hyowon3856@gmail.com>
2024-08-23 14:20:29 +0900

    Inception fin

--- 11_Inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ 11_Inception/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,23 +1,48 @@
+# #!/bin/bash
+
+# if [ -d "/var/lib/mysql/$MARIADB_DATABASE" ]
+# then
+# 	echo "database already exists"
+# else
+# >/tmp/init.sql cat <<EOF
+# USE  mysql;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+# FLUSH PRIVILEGES;
+# CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+# USE  ${MARIADB_DATABASE};
+# CREATE USER ${MARIADB_USER}@'%';
+# GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+# FLUSH PRIVILEGES;
+# EOF
+
+# exec mysqld_safe --init-file /tmp/init.sql
+# mysqld stop
+
+# fi
+
+# exec "$@"
+
 #!/bin/bash
 
 if [ -d "/var/lib/mysql/$MARIADB_DATABASE" ]
 then
-	echo "database already exiests"
+    echo "Database already exists"
 else
->/tmp/init.sql cat <<EOF
-USE  mysql;
+    # Create the SQL initialization script
+    cat <<EOF > /tmp/init.sql
+USE mysql;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
-USE  ${MARIADB_DATABASE};
-CREATE USER ${MARIADB_USER}@'%';
-GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${MARIADB_USER}@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+USE ${MARIADB_DATABASE};
+CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-exec mysqld_safe --init-file /tmp/init.sql
-mysqld stop
-
+    # Start MariaDB with the initialization script
+    exec mysqld_safe --init-file=/tmp/init.sql
 fi
 
+# Execute any command provided as argument to the script
 exec "$@"
